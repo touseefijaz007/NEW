d6ceeb14d0c6880cd032900b287ec304cfafae95
touseefijaz007 <91919978+touseefijaz007@users.noreply.github.com>
2024-01-07 15:49:38 +0500

    Add files via upload

---
+++ AppManager.php
@@ -0,0 +1,22 @@
+[{
+    
+     
+            "PopupEnabled":"true",
+            
+           "App1":"Monster Hunter",
+           "Link1":"com.netmarble.kofg",
+           "Icon1":"https://play-lh.googleusercontent.com/JJ5zXv997qZ0UNzq5TcetDU1cWRxmCisFIeYNlEpyBE2X9TQzVGRTDhqym-YhaGVL9bl",
+            
+           "App2":"Street Fighter",
+           "Link2":"com.netmarble.kofg",
+           "Icon2":"https://play-lh.googleusercontent.com/JJ5zXv997qZ0UNzq5TcetDU1cWRxmCisFIeYNlEpyBE2X9TQzVGRTDhqym-YhaGVL9bl",
+           
+           "App3":"King of Fighters",
+           "Link3":"com.netmarble.kofg",
+           "Icon3":"https://play-lh.googleusercontent.com/JJ5zXv997qZ0UNzq5TcetDU1cWRxmCisFIeYNlEpyBE2X9TQzVGRTDhqym-YhaGVL9bl",
+           
+            "App4":"Candy Crush",
+           "Link4":"com.netmarble.kofg",
+           "Icon4":"https://play-lh.googleusercontent.com/JJ5zXv997qZ0UNzq5TcetDU1cWRxmCisFIeYNlEpyBE2X9TQzVGRTDhqym-YhaGVL9bl"
+
+}]
